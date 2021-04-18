a41a0d6668beff60481dbbe2b5a04e40051bacf4
Philippe Sampont <philippe@sampont.name>
2021-04-18 21:02:54 +0200

    Use Gamma for status

--- js/main.js
+++ js/main.js
@@ -20,16 +20,11 @@ function manageOrientation(event) {
   document.getElementById('beta').innerHTML=Math.round(event.beta);
   document.getElementById('gamma').innerHTML=Math.round(event.gamma);
   
-  if (event.alpha==270 && event.beta==90 && event.gamma==0) {
-    document.getElementById('status').innerHTML = "UP";
-    inc();
-  }
-  else if (event.alpha==0 && event.beta==0 && event.gamma==-90) {
-    document.getElementById('orientation_status').innerHTML = "DOWN";
-    dec();
+  if (event.gamma<=-80 || event.gamma>=80}'") {
+    document.getElementById('orientation_status').innerHTML = "ON";
   }
   else {
-    document.getElementById('orientation_status').innerHTML = "NO";
+    document.getElementById('orientation_status').innerHTML = "OFF";
   }
 }
 
@@ -38,21 +33,16 @@ function manageMotion(event) {
   document.getElementById('ay').innerHTML=Math.round(event.accelerationIncludingGravity.y);
   document.getElementById('az').innerHTML=Math.round(event.accelerationIncludingGravity.z);
   
-  if (event.accelerationIncludingGravity.x >= 8) {
-    if (event.accelerationIncludingGravity.y >= 8) {
-      document.getElementById('acceration_status').innerHTML = "UP";
-      inc();
-    }
-    else if (event.accelerationIncludingGravity.x >= 8 && event.accelerationIncludingGravity.y < 2) {
-      document.getElementById('acceration_status').innerHTML = "DOWN";
-      dec();
-    }
-    else {
-      document.getElementById('acceration_status').innerHTML = "ON";
-    }
+  if (event.accelerationIncludingGravity.y >= 8) {
+    document.getElementById('acceration_status').innerHTML = "UP";
+    if (document.getElementById('orientation_status').innerHTML = "ON") inc();
+  }
+  else if (event.accelerationIncludingGravity.x >= 8 && event.accelerationIncludingGravity.y < 2) {
+    document.getElementById('acceration_status').innerHTML = "DOWN";
+    if (document.getElementById('orientation_status').innerHTML = "ON") dec();
   }
   else {
-    document.getElementById('acceration_status').innerHTML = "OFF";
+    document.getElementById('acceration_status').innerHTML = "NO";
   }
 }
 
